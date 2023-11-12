0b7204af60cbf6131bcdd6728c1f65b6f3959f85
Dmitry <dmitry8912@gmail.com>
2023-11-12 16:33:54 +0300

    0x004

--- rsync.sh
+++ rsync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-hugo && rsync -avz -e 'ssh -p 8022' --delete public/ dmitry@dev-io.ru:/var/www/dev-io.ru/
+rsync -avz -e 'ssh -p 8022' --delete public/ dmitry@dev-io.ru:/var/www/dev-io.ru/
